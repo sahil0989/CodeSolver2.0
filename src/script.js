49feaef28bb43884f13b60163176ac148e94483a
jarvis <sahilbarak111@gmail.com>
2023-04-18 22:40:24 +0530

    changes in js

--- src/script.js
+++ src/script.js
@@ -14,9 +14,9 @@ function talk() {
     document.getElementById('chatLog').innerHTML = user + "<br>";
     if (user in know) {
         document.getElementById('chatLog').innerHTML = know[user] + "<br>";
-        document.getElementById("body").style.paddingTop = "10rem";
-        document.getElementById("glass").style.height = "620px";
-        document.getElementById("glass1").style.height = "620px";
+        // document.getElementById("body").style.paddingTop = "10rem";
+        // document.getElementById("glass").style.height = "620px";
+        // document.getElementById("glass1").style.height = "620px";
     } else {
         document.getElementById('chatLog').innerHTML = "Sorry,I didn't understand <br>";
     }
